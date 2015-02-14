2be6047564f0068ebb8d130894c34e6a217065a8
Scott Weisman <scott@launchpadlab.com>
2015-02-14 00:15:56 -0600

    added current_subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_subdomain
+
+  def current_subdomain
+    @subdomain ||= request.subdomain
+  end
 end
